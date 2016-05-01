7cd7d62a97696d5c53832bcb9c698a282135f5f8
Steven Roland <steven@mojave.io>
2016-05-01 19:52:37 -0400

    Minify scripts
    - Installed gulp-uglify.
    - Ran uglify task, gulp overwrote the original jquery-bootstrap.js file.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,20 @@
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', ['concatScripts'], function() {
   console.log("Default task complete!");
 });
 
 gulp.task('concatScripts', function(){
-  gulp.src([
-    'dev/theme/scripts/jquery.js',
-    'dev/theme/scripts/bootstrap.js'
-  ])
-  .pipe(concat('jquery-bootstrap.js'))
-  .pipe(gulp.dest('build/theme/scripts'));
+  gulp.src(['dev/theme/scripts/jquery.js', 'dev/theme/scripts/bootstrap.js'])
+    .pipe(concat('jquery-bootstrap.js'))
+    .pipe(gulp.dest('build/theme/scripts'));
+});
+
+gulp.task('minifyScripts', function() {
+  gulp.src('build/theme/scripts/jquery-bootstrap.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/theme/scripts'));
 });
