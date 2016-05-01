40c9381d10c63c35aa506853c27ebe551801908a
Steven Roland <steven@mojave.io>
2016-05-01 19:39:31 -0400

    Concat Scripts
    - Installed gulp-concat.
    - Brought in some files to test with.
    - Ran task, gulp created build/theme/scripts/jquery-bootstrap.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 'use strict';
 
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
-gulp.task('default', ['hello'], function() {
+gulp.task('default', ['concatScripts'], function() {
   console.log("Default task complete!");
 });
 
-gulp.task('hello', function(){
-  console.log("Hello, beautiful!");
+gulp.task('concatScripts', function(){
+  gulp.src([
+    'dev/theme/scripts/jquery.js',
+    'dev/theme/scripts/bootstrap.js'
+  ])
+  .pipe(concat('jquery-bootstrap.js'))
+  .pipe(gulp.dest('build/theme/scripts'));
 });
